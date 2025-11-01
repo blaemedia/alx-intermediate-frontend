01ce75c840dd946183fed8cc3164020ddd309a59
Emmanuel <blaemedia26@gmail.com>
2025-11-01 15:22:59 +0100

    Creating a Responsive CSS Grid Layout with Tailwind CSS

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"], // or ["./1-index.html"] if you only have one file
   theme: {
     extend: {},
   },
