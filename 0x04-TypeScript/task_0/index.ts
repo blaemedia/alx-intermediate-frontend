24e83386b7da6fe8dafc7b99fd2987b5f772a2d1
Emmanuel <blaemedia26@gmail.com>
2025-11-05 06:51:57 +0100

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/index.ts
@@ -0,0 +1,11 @@
+let number = 42;
+let myName = "Hello, TypeScript!";
+let isActive = true;
+
+
+console.log(`Number: ${number}`);
+console.log(`Name: ${myName}`);
+console.log(`Is Active: ${isActive}`);
+
+
+console.log(`Number: ${number}`);
